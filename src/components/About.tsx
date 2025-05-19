70b5ebc3200bf4f357759c048634c60b7520b265
EduWarden <nurgissabazaraly@gmail.com>
2025-05-19 14:43:58 +0500

    edited about me and removed button download cv

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -244,18 +244,11 @@ const About = () => {
 
               <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
                 <Button
-                    variant="default"
-                    className="w-full lg:w-auto bg-gradient-to-r from-[#6366f1] to-[#a855f7] hover:opacity-90 transition-all duration-300 hover:scale-105 flex items-center justify-center lg:justify-start gap-2 shadow-lg hover:shadow-xl animate-bounce-slow"
-                    data-aos="fade-up"
-                    data-aos-duration="800"
-                >
-                  <FileText className="w-4 h-4 sm:w-5 sm:h-5" /> Download CV
-                </Button>
-                <Button
                     variant="outline"
                     className="w-full lg:w-auto sm:px-6 py-2 sm:py-3 rounded-lg border border-[#a855f7]/50 text-[#a855f7] font-medium transition-all duration-300 hover:scale-105 flex items-center justify-center lg:justify-start gap-2 hover:bg-[#a855f7]/10 animate-bounce-slow delay-200"
                     data-aos="fade-up"
                     data-aos-duration="1000"
+                    onClick={() => window.location.href = '#portfolio'}
                 >
                   <Code className="w-4 h-4 sm:w-5 sm:h-5" /> View Projects
                 </Button>
